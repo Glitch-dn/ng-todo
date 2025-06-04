de260ab3fa9b5fd8a9c5bdb4d1ee883698c8c19e
Glitch-dn <danielegambucci.lab@gmail.com>
2025-06-04 11:32:51 +0200

    switchList-button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { TodoListComponent } from './components/todo-list/todo-list.component';
 import { AutoListComponent } from "./components/auto-list/auto-list.component";
 
+type Section = 'todo' | 'auto' | 'carrello'; // Definisco un tipo per le sezioni, così posso usarlo per il controllo dei tipi
+
 @Component({
   selector: 'app-root',
   imports: [TodoListComponent, AutoListComponent],
@@ -10,4 +12,10 @@ import { AutoListComponent } from "./components/auto-list/auto-list.component";
 })
 export class AppComponent {
   title = 'ng-todo';
+
+  selectedSection: Section = 'todo'; //in partenza lista todo. scrivo così al tipo almeno se sbaglio nel click mi da errore, altrimenti non mi accorgo e la lista non apparirebbe
+
+  sectionChange(section: Section) {
+    this.selectedSection = section;
+  }
 }
